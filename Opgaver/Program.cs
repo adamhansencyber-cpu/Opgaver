596dce6a758804c8eb4c583d8e87111dd40a1d46
Adam Hansen <adamhansencyber@gmail.com>
2026-01-15 13:42:27 +0100

    Min Metode

--- Opgaver/Program.cs
+++ Opgaver/Program.cs
@@ -7,5 +7,10 @@
             Console.WriteLine("Hello, World!"); //en kommentar
 
         }
+        static void MinMetode()
+        {
+            //en kommentar
+            Console.WriteLine("Hej med dig!");
+        }
     }
 }
